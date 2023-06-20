83102dd067b2784e1b7ad8f0922652a025e95a69
Alex Riasanovsky <a.riasanovsky@gmail.com>
2023-06-20 09:31:20 -0400

    published seed structs

--- src/seed/mod.rs
+++ src/seed/mod.rs
@@ -6,11 +6,11 @@ const BASE: i64 = N_LETTERS as i64;
 static ALPHABET: &[u8; N_LETTERS] = b"0123456789ABCDEFGHIJKLMNPQRSTUVWXYZ";
 
 #[derive(Debug)]
-pub(crate) struct Seed {
+pub struct Seed {
     pub(crate) seed: i64,
 }
 
 #[derive(Debug)]
-pub(crate) struct SeedString {
+pub struct SeedString {
     seed: String,
 }
